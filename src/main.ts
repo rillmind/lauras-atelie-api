a38c30ba6eb477422207da67de79b10507232136
Raul Holanda Lopes <raulzc00@gmail.com>
2026-04-02 03:34:07 -0300

    fix: ajustante para deploy

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   const port = process.env.PORT || 3001;
   await app.listen(port, "0.0.0.0");
